311da71a28cdff5ea60de811573e12dc37826b16
YUZAMIN <untmd.crmn@gmail.com>
2023-03-30 02:58:10 +0900

    feat: Layout 컴포넌트를 생성하라
    - 공통적으로 적용될 레이아웃 CSS를 정의
    Fixes: #3

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Layout } from '../components/layouts/Layout'
 import { HomePage } from '../pages/HomePage'
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
