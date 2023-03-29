ad8d50eae549b20df2610898b32fed489edf485e
YUZAMIN <untmd.crmn@gmail.com>
2023-03-30 02:53:11 +0900

    feat: theme 파일을 생성하라
    Fixes: #3

---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+const color = {
+  primary: '',
+  secondary: '',
+  tertiary: '',
+  white: '#fff',
+  black: '#0b0b0b',
+  grey: '#6e6e6e',
+  lightGrey: '#d1d1d1',
+  navy: '#0000aa',
+  red: '#ff0000',
+}
+
+const layout = {}
+
+const device = {}
+
+export const theme = {
+  color,
+}
