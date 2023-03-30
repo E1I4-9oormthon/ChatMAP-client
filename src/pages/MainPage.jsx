e2b4309b408c6892d73fece1b60ab5171bc9627d
YUZAMIN <untmd.crmn@gmail.com>
2023-03-30 14:31:02 +0900

    feat: 메인 페이지 컴포넌트를 생성하라
    Fixes: #16

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import { theme } from '../styles/theme'
+
+export const MainPage = () => {
+  return <Wrapper>HELLO</Wrapper>
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 100%;
+  padding: 0 1rem 1rem;
+  box-sizing: border-box;
+`
