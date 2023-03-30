c2aea7e874902570d71d5358aab9ce9e943c883e
YUZAMIN <untmd.crmn@gmail.com>
2023-03-30 12:01:32 +0900

    fix: axios baseURL 설정을 제거하라
    Fixes: #8

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import axios from 'axios'
 import App from './App'
-
-axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
