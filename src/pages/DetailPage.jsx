d0b538822845b4a54d026611975fba8d820404ab
YUZAMIN <untmd.crmn@gmail.com>
2023-03-30 17:52:36 +0900

    feat: 디테일 페이지 UI 수정하라
    Fixes: #22

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,75 +1,89 @@
-import { useState, useEffect, useCallback, useRef } from 'react'
 import styled from 'styled-components'
 import { theme } from '../styles/theme'
 import TitleImg from '../assets/images/title_img.png'
+import MapImg from '../assets/images/map_img.png'
+import CalendarIcon from '../assets/icons/calendar_ico.png'
+import GenderIcon from '../assets/icons/gender_ico.png'
+import LocationIcon from '../assets/icons/location_ico.png'
 
 export const DetailPage = () => {
-  return <Wrapper>DETAIL</Wrapper>
+  return (
+    <Wrapper>
+      <ProfileImageWrapper>
+        <ProfileImage src={TitleImg} />
+      </ProfileImageWrapper>
+      <Title>저녁에 올레길 산책하실 분</Title>
+      <InfoWrapper>
+        <InfoBox>
+          <InfoIcon src={CalendarIcon} />
+          온평 - 표선 올레 A코스
+        </InfoBox>
+        <InfoBox>
+          <InfoIcon src={GenderIcon} />
+          남성분과 동행할래요
+        </InfoBox>
+        <InfoBox>
+          <InfoIcon src={LocationIcon} />
+          2023.03.30
+        </InfoBox>
+      </InfoWrapper>
+      <MapImage src={MapImg} />
+      <CommentListWrapper>
+        <CommentListTitle>같이 동행하고 싶어요</CommentListTitle>
+      </CommentListWrapper>
+    </Wrapper>
+  )
 }
-const Tab = styled.button`
-  cursor: pointer;
-  width: 50%;
-  border: 0;
-  outline: 0;
-  ${({ active }) =>
-    active &&
-    `
-    font-weight: 700;
-    border-bottom: 2px solid ${theme.color.primary};
-  `}
+
+const InfoWrapper = styled.div`
+  padding: 1.5rem 0;
 `
-const TabBox = styled.div`
-  display: flex;
-  width: 100%;
+
+const InfoIcon = styled.img`
+  width: 2rem;
+  margin-right: 1rem;
 `
-const TabContainer = styled.div`
+const InfoBox = styled.div`
   display: flex;
-  justify-content: center;
-  width: 100%;
+  align-items: center;
+  padding: 0.2 0;
 `
 
 const Wrapper = styled.div`
-  background: ${theme.color.lightGrey};
+  background: ${theme.color.white};
   height: calc(100% - 2rem);
   padding: 1rem;
   overflow: scroll;
   word-break: break-all;
 `
 
-const SuggestBox = styled.div`
-  margin: 1rem 0;
-  background: ${theme.color.white};
-  border: 1px solid ${theme.color.grey};
-  border-radius: 10px;
-  padding: 1rem;
+const ProfileImageWrapper = styled.div`
   display: flex;
-  align-items: center;
+  justify-content: center;
+  padding: 1rem 0;
 `
 
 const ProfileImage = styled.img`
-  width: 3.5rem;
-  height: 3.5rem;
+  width: 6rem;
+  height: 6rem;
   border-radius: 50%;
 `
-const ContentsWrapper = styled.div`
-  width: 100%;
-  padding: 1rem 0.5rem;
-`
-const UserName = styled.div`
-  font-size: 20px;
+
+const Title = styled.div`
+  font-size: 18px;
   font-weight: 700;
-  margin-bottom: 0.5rem;
+  margin-top: 1rem;
 `
-const UserFavoriteGender = styled.div`
-  display: flex;
-  font-size: 15px;
-  justify-content: space-between;
+const MapImage = styled.img`
+  margin-bottom: 1rem;
 `
 
-const PostListBottom = styled.div`
-  ${({ continueFetching }) =>
-    !continueFetching &&
-    `
-    display: none !important;
-  `}
+const CommentListWrapper = styled.div`
+  border-top: 10px solid ${theme.color.lightGrey};
+  padding: 2rem 0;
+`
+
+const CommentListTitle = styled.div`
+  font-size: 18px;
+  font-weight: 700;
 `
